ee039f763f0eea3de9cdb25ccabe38a7b5e8cf19
Myles Braithwaite <me@mylesbraithwaite.com>
2016-07-27 11:22:17 -0400

    Fixing a minor PEP8 issue.

--- fabfile.py
+++ fabfile.py
@@ -8,6 +8,7 @@ env.use_ssh_config = True
 # Remote Directories
 env.html_dir = '/srv/www/pycon.ca/2016/html/'
 
+
 @task
 def deploy():
     rsync_project(remote_dir=env.html_dir, local_dir='./comingsoon/')
