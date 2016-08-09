492adc6f16d7f5f98f58606dbc4bf9b4374af23b
Myles Braithwaite <me@mylesbraithwaite.com>
2016-08-09 18:57:53 -0400

    Added some empty files.

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,4 @@
-from fabric.api import task, env, local
+from fabric.api import task, env, local, require
 from fabric.contrib.project import rsync_project
 
 from web.config import Config
@@ -10,14 +10,22 @@ env.use_ssh_config = True
 
 @task
 def stag():
-    env.html_dir = '/srv/www/pycon.ca/2016/html/'
+    env.environment = 'staging'
+    env.html_dir = '/srv/www/pycon.ca/staging.2016/html/'
 
 
 @task
 def prod():
-    env.html_dir '/srv/www/pycon.ca/staging.2016/html/'
+    env.environment = 'production'
+    env.html_dir = '/srv/www/pycon.ca/2016/html/'
 
 
 @task
 def deploy():
-    rsync_project(remote_dir=env.html_dir, local_dir='./comingsoon/')
+    require('environment')
+
+    # Build the static website.
+    local('python manage.py freeze')
+
+    # rsync the website to the server.
+    rsync_project(remote_dir=env.html_dir, local_dir='./build/')
