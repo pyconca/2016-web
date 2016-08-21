6ea511fe670818998dacd36080fc9c5de117315c
Myles Braithwaite <me@mylesbraithwaite.com>
2016-08-21 15:51:25 -0400

    Added some empty template files.

--- web/views/schedule.py
+++ web/views/schedule.py
@@ -5,4 +5,9 @@ schedule = Blueprint('schedule', __name__)
 
 @schedule.route('/')
 def index():
-    return render_template('pages/schedule_index.html')
+    return render_template('pages/schedule/index.html')
+
+
+@schedule.rote('/<path:slug>/')
+def talk():
+    return render_template('pages/schedule/talk.html')
