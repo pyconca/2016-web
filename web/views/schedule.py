db44562f0f8c3f14449e977a14aac52968715122
Myles Braithwaite <me@mylesbraithwaite.com>
2016-10-03 10:41:25 -0400

    Showing the sponsor logos.

--- web/views/schedule.py
+++ web/views/schedule.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template, g
+from datetime import datetime
+
+from icalendar import Calendar, Event
+
+from flask import Blueprint, render_template, jsonify, g
 
 from web.utils import get_data_file, get_markdown_file
 
@@ -24,3 +28,18 @@ def talk(slug):
 
     return render_template('pages/schedule/talk.html', content=content,
                            meta=meta)
+
+
+@schedule.route('/<string:slug>/json/')
+def talk_json(slug):
+    description, content = get_markdown_file('talks/{}'.format(slug), 'en')
+    content['description'] = description
+    return jsonify(content)
+
+
+@schedule.route('/<string:slug>/ics/')
+def talk_ics(slug):
+    description, content = get_markdown_file('talks/{}'.format(slug), 'en')
+
+    cal = Calendar()
+    cal.add('prodid', '-//PyCon Canada 2016//2016.pycon.ca')
