04e8933f19ebb73ac64c89cbb451dc7444a64e02
Myles Braithwaite <me@mylesbraithwaite.com>
2016-10-03 11:17:19 -0400

    Did some work on the calendar stuff.

--- web/utils.py
+++ web/utils.py
@@ -63,8 +63,6 @@ def get_markdown_file(name, lang='en'):
 
     md = markdown.Markdown(['markdown.extensions.meta'])
 
-    md = markdown.Markdown(['markdown.extensions.meta'])
-
     with codecs.open(filepath, mode='r', encoding="utf-8") as f:
         html = md.convert(f.read())
 
--- web/views/schedule.py
+++ web/views/schedule.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import pytz
 from icalendar import Calendar, Event
 
-from flask import Blueprint, render_template, jsonify, g
+from flask import Blueprint, render_template, jsonify, Response, url_for, g
 
 from web.utils import get_data_file, get_markdown_file
 
@@ -42,12 +42,14 @@ def talk_json(slug):
 def talk_ics(slug):
     description, content = get_markdown_file('talks/{}'.format(slug), 'en')
 
-    start_time = datetime.strptime('{0} {1}'.format(content['date'],
-                                                    content['start_time']),
+    tz = pytz.timezone('Canada/Eastern')
+
+    start_time = datetime.strptime('{0} {1}'.format(content['date'][0],
+                                                    content['start_time'][0]),
                                    '%Y-%m-%d %H:%M:%S')
 
-    end_time = datetime.strptime('{0} {1}'.format(content['date'],
-                                                  content['end_time']),
+    end_time = datetime.strptime('{0} {1}'.format(content['date'][0],
+                                                  content['end_time'][0]),
                                  '%Y-%m-%d %H:%M:%S')
 
     cal = Calendar()
@@ -55,8 +57,24 @@ def talk_ics(slug):
     cal.add('version', '2.0')
 
     event = Event()
-    event.add('summary', "{0} with {1}".format(content['title'],
-                                               content['speakers']))
-    event.add('dtstart', start_time)
-    event.add('dtend', end_time)
-    event.add('dtstamp', start_time)
+    event.add('summary', "{0} with {1}".format(content['title'][0],
+                                               content['speakers'][0]))
+    event.add('dtstart', tz.localize(start_time))
+    event.add('dtend', tz.localize(end_time))
+    event.add('dtstamp', tz.localize(start_time))
+
+    event.add('uid', slug)
+
+    tpl_url = 'https://2016.pycon.ca{0}'
+    event.add('url', tpl_url.format(url_for('schedule.talk', slug=slug)))
+
+    cal.add_component(event)
+
+    headers = {
+        'Content-Disposition': 'attachment;filename={0}.ics'.format(slug)
+    }
+
+    return Response(response=cal.to_ical(),
+                    status=200,
+                    mimetype='text/x-calendar',
+                    headers=headers)
