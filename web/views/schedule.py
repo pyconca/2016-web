9087ba9429bccf5370636bed59e25d1d2dc4de5a
Myles Braithwaite <me@mylesbraithwaite.com>
2016-10-03 10:47:42 -0400

    Added a JSON feed and started working on the iCal feed.

--- web/views/schedule.py
+++ web/views/schedule.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+import pytz
 from icalendar import Calendar, Event
 
 from flask import Blueprint, render_template, jsonify, g
@@ -41,5 +42,21 @@ def talk_json(slug):
 def talk_ics(slug):
     description, content = get_markdown_file('talks/{}'.format(slug), 'en')
 
+    start_time = datetime.strptime('{0} {1}'.format(content['date'],
+                                                    content['start_time']),
+                                   '%Y-%m-%d %H:%M:%S')
+
+    end_time = datetime.strptime('{0} {1}'.format(content['date'],
+                                                  content['end_time']),
+                                 '%Y-%m-%d %H:%M:%S')
+
     cal = Calendar()
     cal.add('prodid', '-//PyCon Canada 2016//2016.pycon.ca')
+    cal.add('version', '2.0')
+
+    event = Event()
+    event.add('summary', "{0} with {1}".format(content['title'],
+                                               content['speakers']))
+    event.add('dtstart', start_time)
+    event.add('dtend', end_time)
+    event.add('dtstamp', start_time)
