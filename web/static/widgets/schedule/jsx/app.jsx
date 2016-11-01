86aae035233c2f7297efe2c4b4c95f62ce24eb8f
Juti Noppornpitak <jnopporn@shiroyuki.com>
2016-11-01 02:21:03 -0400

    Integrated the schedule widget with the main page.

--- web/static/widgets/schedule/jsx/app.jsx
+++ web/static/widgets/schedule/jsx/app.jsx
@@ -1,3 +1,6 @@
+import React              from 'react';
+import ReactDOM, {render} from 'react-dom';
+
 import Analyzer from './data/analyzer.jsx';
 
 let combinedData = require('../../../../data/schedule.json');
@@ -8,10 +11,11 @@ let analyzer     = new Analyzer();
 let days         = analyzer.objectifySessions(scheduleData, talkData);
 let timeline     = analyzer.analyzeTimeline(days);
 
-import React              from 'react';
-import ReactDOM, {render} from 'react-dom';
-import Core               from './com/core.jsx';
+import Core from './com/core.jsx';
 
 for (let dom of document.querySelectorAll('.widget-schedule')) {
     ReactDOM.render(<Core timeline={ timeline } rooms={ roomData } />, dom);
 }
+
+document.querySelectorAll('.schedule__day').remove();
+document.querySelectorAll('.schedule').remove();
